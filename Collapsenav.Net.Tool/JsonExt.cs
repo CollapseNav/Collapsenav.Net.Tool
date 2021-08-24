50249b01bb92403c71eb13116d610ac4d68b6a7e
CollapseNav <jwpwdwd@163.com>
2021-08-24 13:21:26 +0800

    perf: 添加时间相关扩展
    添加时间相关扩展
    json扩展中加了点注释

--- Collapsenav.Net.Tool/JsonExt.cs
+++ Collapsenav.Net.Tool/JsonExt.cs
@@ -5,8 +5,20 @@ namespace Collapsenav.Net.Tool
 {
     public static class JsonExt
     {
+        /// <summary>
+        /// Json字符串转为对象
+        /// </summary>
         public static T ToObj<T>(this string str, JsonSerializerOptions options = null) => JsonSerializer.Deserialize<T>(str, options);
+        /// <summary>
+        /// Json字符串转为对象集合
+        /// </summary>
         public static IEnumerable<T> ToObjCollection<T>(this string str, JsonSerializerOptions options = null) => JsonSerializer.Deserialize<IEnumerable<T>>(str, options);
+        /// <summary>
+        /// 对象转为Json字符串
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
         public static string ToJson(this object obj, JsonSerializerOptions options = null) => JsonSerializer.Serialize(obj, options);
     }
 }
