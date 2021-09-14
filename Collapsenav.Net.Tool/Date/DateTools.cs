07f03cfa3244ab21311019b9bfaaefc36d028ee1
CollapseNav <jwpwdwd@163.com>
2021-09-15 01:07:35 +0800

    feat: 测试url是否可达，添加测试覆盖率
    使用ping测试url的域名是否可达
    尝试添加了测试覆盖率的配置

--- Collapsenav.Net.Tool/Date/DateTools.cs
+++ Collapsenav.Net.Tool/Date/DateTools.cs
@@ -4,17 +4,24 @@ namespace Collapsenav.Net.Tool
 {
     public class DateTools
     {
+        /// <summary>
+        /// DateTime 转时间戳
+        /// </summary>
         public static long ToTimeStamp(DateTime time)
         {
             return new DateTimeOffset(time).ToUnixTimeMilliseconds();
         }
-
+        /// <summary>
+        /// 时间戳转 DateTime
+        /// </summary>
         public static DateTime ToDateTime(long? timestamp = null)
         {
             if (!timestamp.HasValue || timestamp <= 0) return DateTime.Now;
             return DateTimeOffset.FromUnixTimeMilliseconds(timestamp.Value).DateTime;
         }
-
+        /// <summary>
+        /// 保留时间到 某个指定的leve
+        /// </summary>
         public static DateTime TransformDateTime(DateTime time, DateLevel level)
         {
             var trans = level switch
