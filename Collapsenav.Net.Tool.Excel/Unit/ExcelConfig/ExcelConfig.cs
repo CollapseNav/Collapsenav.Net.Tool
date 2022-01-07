cc8fd1fc146d2b97a71be9d47cf744889ba8980b
CollapseNav <jwpwdwd@163.com>
2022-01-07 23:51:49 +0800

    feat: 尝试统一EPPlus和NPOI的调用
    尝试统一EPPlus和NPOI的调用, 方便后续的封装
    希望性能不会下降太多

---
+++ Collapsenav.Net.Tool.Excel/Unit/ExcelConfig/ExcelConfig.cs
@@ -0,0 +1,64 @@
+namespace Collapsenav.Net.Tool.Excel;
+
+/// <summary>
+/// 表格配置
+/// </summary>
+public class ExcelConfig<T, CellConfig> where CellConfig : BaseCellOption<T>
+{
+    public ExcelConfig()
+    {
+        FieldOption = new List<CellConfig>();
+    }
+    /// <summary>
+    /// 依据表头的设置
+    /// </summary>
+    public virtual IEnumerable<CellConfig> FieldOption { get; set; }
+    /// <summary>
+    /// 表格数据
+    /// </summary>
+    public IEnumerable<T> Data { get; set; }
+    /// <summary>
+    /// 获取表头
+    /// </summary>
+    public virtual IEnumerable<string> Header { get => FieldOption.Select(item => item.ExcelField); }
+    /// <summary>
+    /// 根据给出的表头筛选options
+    /// </summary>
+    public void FilterOptionByHeaders(IEnumerable<string> headers)
+    {
+        FieldOption = FilterOptionByHeaders(FieldOption, headers);
+    }
+    /// <summary>
+    /// 根据给出的表头筛选options
+    /// </summary>
+    public static IEnumerable<CellConfig> FilterOptionByHeaders(IEnumerable<CellConfig> options, IEnumerable<string> headers)
+    {
+        return options.Where(item => headers.Any(head => head == item.ExcelField));
+    }
+    /// <summary>
+    /// 添加普通单元格设置
+    /// </summary>
+    public virtual void Add(CellConfig option)
+    {
+        FieldOption = FieldOption.Append(option);
+    }
+    /// <summary>
+    /// check条件为True时添加普通单元格设置
+    /// </summary>
+    public virtual void AddIf(bool check, CellConfig option)
+    {
+        if (check) Add(option);
+    }
+    /// <summary>
+    /// 移除指定单元格配置
+    /// </summary>
+    public virtual ExcelConfig<T, CellConfig> Remove(string field)
+    {
+        FieldOption = FieldOption.Where(item => item.ExcelField != field);
+        return this;
+    }
+}
+
+public class ExcelConfig : ExcelConfig<object, BaseCellOption<object>>
+{
+}
