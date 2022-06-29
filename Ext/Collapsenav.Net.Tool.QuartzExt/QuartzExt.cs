17aa8ec8b2520e337c6c1576495b7be3fcd600a4
CollapseNav <jwpwdwd@163.com>
2022-06-30 00:57:18 +0800

    feat: 修改方法名称，突出cron   添加删除job的扩展功能

--- Ext/Collapsenav.Net.Tool.QuartzExt/QuartzExt.cs
+++ Ext/Collapsenav.Net.Tool.QuartzExt/QuartzExt.cs
@@ -7,27 +7,25 @@ public static class QuartzExt
 {
     public static async Task ScheduleJob<Job>(this IScheduler scheduler, JobKey jkey, TriggerKey tkey, string cron) where Job : IJob
     {
-        await scheduler.ScheduleJob(QuartzTool.CreateJob<Job>(jkey), QuartzTool.CreateTrigger(tkey, cron));
+        await scheduler.ScheduleJob(QuartzTool.CreateJob<Job>(jkey), QuartzTool.CreateCronTrigger(tkey, cron));
     }
-
     public static async Task ScheduleJob<Job>(this IScheduler scheduler, string name, string group, string cron) where Job : IJob
     {
-        await scheduler.ScheduleJob(QuartzTool.CreateJob<Job>(name, group), QuartzTool.CreateTrigger(name, group, cron));
+        await scheduler.ScheduleJob(QuartzTool.CreateJob<Job>(name, group), QuartzTool.CreateCronTrigger(name, group, cron));
     }
-
     public static async Task ScheduleJob<Job>(this IScheduler scheduler, string name, string cron) where Job : IJob
     {
-        await scheduler.ScheduleJob(QuartzTool.CreateJob<Job>(name), QuartzTool.CreateTrigger(name, cron));
+        await scheduler.ScheduleJob(QuartzTool.CreateJob<Job>(name), QuartzTool.CreateCronTrigger(name, cron));
     }
     public static async Task ScheduleJobs<Job>(this IScheduler scheduler, string name, IEnumerable<string> crons) where Job : IJob
     {
-        var jobAndTriggers = QuartzTool.CreateJobAndTriggers<Job>(name, name, crons);
+        var jobAndTriggers = QuartzTool.CreateJobAndCronTriggers<Job>(name, name, crons);
         foreach (var item in jobAndTriggers)
             await scheduler.ScheduleJob(item.Job, item.Trigger);
     }
     public static async Task ScheduleJobs<Job>(this IScheduler scheduler, string name, string group, IEnumerable<string> crons) where Job : IJob
     {
-        var jobAndTriggers = QuartzTool.CreateJobAndTriggers<Job>(name, group, crons);
+        var jobAndTriggers = QuartzTool.CreateJobAndCronTriggers<Job>(name, group, crons);
         foreach (var item in jobAndTriggers)
             await scheduler.ScheduleJob(item.Job, item.Trigger);
     }
@@ -40,11 +38,37 @@ public static class QuartzExt
     public static async Task ScheduleJobs<Job>(this IScheduler scheduler, IEnumerable<string> crons) where Job : IJob
     {
         var name = typeof(Job).Name;
-        var jobAndTriggers = QuartzTool.CreateJobAndTriggers<Job>(name, name, crons);
+        var jobAndTriggers = QuartzTool.CreateJobAndCronTriggers<Job>(name, name, crons);
+        foreach (var item in jobAndTriggers)
+            await scheduler.ScheduleJob(item.Job, item.Trigger);
+    }
+    /// <summary>
+    /// 不指定任务名称, 使用泛型名称作为任务的name和group
+    /// </summary>
+    /// <remarks>
+    /// 反正就算需要指定名称, 最后和泛型名称也不会有太大区别, 不如干脆放手!
+    /// </remarks>
+    public static async Task ScheduleJobs(this IScheduler scheduler, Type type, IEnumerable<string> crons)
+    {
+        var name = type.Name;
+        var jobAndTriggers = QuartzTool.CreateJobAndCronTriggers(type, name, name, crons);
         foreach (var item in jobAndTriggers)
             await scheduler.ScheduleJob(item.Job, item.Trigger);
     }
 
+    public static async Task DeleteJob(this IScheduler scheduler, JobKey jkey)
+    {
+        await scheduler.DeleteJob(jkey);
+    }
+
+    public static async Task DeleteJobs(this IScheduler scheduler, string group)
+    {
+        var keys = await scheduler.GetJobKeys(GroupMatcher<JobKey>.GroupEquals(group));
+        if (keys.IsEmpty())
+            return;
+        foreach (var key in keys)
+            await scheduler.DeleteJob(key);
+    }
 
     public static async Task RescheduleJob(this IScheduler scheduler, ITrigger trigger)
     {
--- Ext/Collapsenav.Net.Tool.QuartzExt/QuartzTool.cs
+++ Ext/Collapsenav.Net.Tool.QuartzExt/QuartzTool.cs
@@ -3,7 +3,7 @@ using Quartz;
 namespace Collapsenav.Net.Tool.Ext;
 public class QuartzTool
 {
-    public static ITrigger CreateTrigger(TriggerKey tkey, string cron)
+    public static ITrigger CreateCronTrigger(TriggerKey tkey, string cron)
     {
         return TriggerBuilder.Create()
         .StartNow()
@@ -16,16 +16,16 @@ public class QuartzTool
     /// </summary>
     /// <param name="name">name 和 group 都是 key</param>
     /// <param name="cron"></param>
-    public static ITrigger CreateTrigger(string name, string cron)
+    public static ITrigger CreateCronTrigger(string name, string cron)
     {
-        return CreateTrigger(new TriggerKey(name, name), cron);
+        return CreateCronTrigger(new TriggerKey(name, name), cron);
     }
     /// <summary>
     /// 获取一个trigger
     /// </summary>
-    public static ITrigger CreateTrigger(string name, string group, string cron)
+    public static ITrigger CreateCronTrigger(string name, string group, string cron)
     {
-        return CreateTrigger(new TriggerKey(name, group), cron);
+        return CreateCronTrigger(new TriggerKey(name, group), cron);
     }
 
 
@@ -65,11 +65,11 @@ public class QuartzTool
     /// <para>如: 传入 name,group</para>
     /// <para>则会生成 name_1.group, name_2.group, name_3.group ... 这样的key</para>
     /// </remarks>
-    public static IEnumerable<ITrigger> CreateTriggers(string name, string group, IEnumerable<string> crons)
+    public static IEnumerable<ITrigger> CreateCronTriggers(string name, string group, IEnumerable<string> crons)
     {
         return crons.Select((item, index) =>
         {
-            return CreateTrigger(new TriggerKey($"{name}_{index}", $"{group}"), item);
+            return CreateCronTrigger(new TriggerKey($"{name}_{index}", $"{group}"), item);
         });
     }
     /// <summary>
@@ -80,43 +80,58 @@ public class QuartzTool
     /// <para>如: 传入 name</para>
     /// <para>则会生成 name_1.name, name_2.name, name_3.name ... 这样的key</para>
     /// </remarks>
-    public static IEnumerable<ITrigger> CreateTriggers(string name, IEnumerable<string> crons)
+    public static IEnumerable<ITrigger> CreateCronTriggers(string name, IEnumerable<string> crons)
     {
-        return CreateTriggers(name, name, crons);
+        return CreateCronTriggers(name, name, crons);
     }
 
 
 
-    public static JobAndTriggerItem CreateJobAndTrigger<Job>(string name, string cron) where Job : IJob
+    public static JobAndTriggerItem CreateJobAndCronTrigger<Job>(string name, string cron) where Job : IJob
     {
-        return CreateJobAndTrigger<Job>(new JobKey(name, name), new TriggerKey(name, name), cron);
+        return CreateJobAndCronTrigger<Job>(new JobKey(name, name), new TriggerKey(name, name), cron);
     }
-    public static JobAndTriggerItem CreateJobAndTrigger<Job>(string name, string group, string cron) where Job : IJob
+    public static JobAndTriggerItem CreateJobAndCronTrigger<Job>(string name, string group, string cron) where Job : IJob
     {
-        return CreateJobAndTrigger<Job>(new JobKey(name, group), new TriggerKey(name, group), cron);
+        return CreateJobAndCronTrigger<Job>(new JobKey(name, group), new TriggerKey(name, group), cron);
     }
-    public static JobAndTriggerItem CreateJobAndTrigger<Job>(JobKey jobKey, TriggerKey triggerKey, string cron) where Job : IJob
+    public static JobAndTriggerItem CreateJobAndCronTrigger<Job>(JobKey jobKey, TriggerKey triggerKey, string cron) where Job : IJob
     {
         return new JobAndTriggerItem
         {
-            Trigger = CreateTrigger(triggerKey, cron),
+            Trigger = CreateCronTrigger(triggerKey, cron),
             Job = CreateJob<Job>(jobKey)
         };
     }
 
 
-    public static IEnumerable<JobAndTriggerItem> CreateJobAndTriggers<Job>(string name, IEnumerable<string> crons) where Job : IJob
+    public static IEnumerable<JobAndTriggerItem> CreateJobAndCronTriggers<Job>(string name, IEnumerable<string> crons) where Job : IJob
     {
-        return CreateJobAndTriggers<Job>(name, name, crons);
+        return CreateJobAndCronTriggers<Job>(name, name, crons);
     }
-    public static IEnumerable<JobAndTriggerItem> CreateJobAndTriggers<Job>(string name, string group, IEnumerable<string> crons) where Job : IJob
+    public static IEnumerable<JobAndTriggerItem> CreateJobAndCronTriggers<Job>(string name, string group, IEnumerable<string> crons) where Job : IJob
     {
-        return CreateTriggers(name, group, crons).Select(item =>
+        return CreateCronTriggers(name, group, crons).Select(item =>
         {
             return new JobAndTriggerItem
             {
                 Trigger = item,
                 Job = CreateJob<Job>(item.Key.Name, item.Key.Group)
+            };
+        });
+    }
+    public static IEnumerable<JobAndTriggerItem> CreateJobAndCronTriggers(Type type, string name, IEnumerable<string> crons)
+    {
+        return CreateJobAndCronTriggers(type, name, name, crons);
+    }
+    public static IEnumerable<JobAndTriggerItem> CreateJobAndCronTriggers(Type type, string name, string group, IEnumerable<string> crons)
+    {
+        return CreateCronTriggers(name, group, crons).Select(item =>
+        {
+            return new JobAndTriggerItem
+            {
+                Trigger = item,
+                Job = CreateJob(type, item.Key.Name, item.Key.Group)
             };
         });
     }
