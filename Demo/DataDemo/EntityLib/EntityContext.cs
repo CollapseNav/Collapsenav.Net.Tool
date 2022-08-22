d29f044c47cac0b3cde53b706760dd955e89476f
CollapseNav <jwpwdwd@163.com>
2022-08-22 23:14:42 +0800

    [Demo] 添加简单 Data 包读写数据库的 demo Fixes #8

---
+++ Demo/DataDemo/EntityLib/EntityContext.cs
@@ -0,0 +1,12 @@
+using Collapsenav.Net.Tool.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataDemo.EntityLib;
+
+public class EntityContext : DbContext
+{
+    public DbSet<FirstEntity> FirstEntity { get; set; }
+    public EntityContext(DbContextOptions<EntityContext> options) : base(options)
+    {
+    }
+}
