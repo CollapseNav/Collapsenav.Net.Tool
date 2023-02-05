7421229460db2fcffd4d18ec398201b950cc3f96
CollapseNav <jwpwdwd@163.com>
2023-02-05 17:18:48 +0800

    添加简单的Redis扩展包 Fixes #4

--- Ext/Collapsenav.Net.Tool.Redis/IRedisCache.cs
+++ Ext/Collapsenav.Net.Tool.Redis/IRedisCache.cs
@@ -2,7 +2,20 @@ namespace Collapsenav.Net.Tool.Ext;
 
 public interface IRedisCache : ICache
 {
+    string BuildHashKey(string key);
     IEnumerable<string> GetKeys(string pattern, long count = 10);
     long SetHash(string key, string field, object value);
     string GetHash(string key, string field);
+    T GetHash<T>(string key, string field);
+    Dictionary<string, string> GetHashAll(string key);
+    Dictionary<string, T> GetHashAll<T>(string key);
+    long SetList(string key, params object[] value);
+    string GetList(string key);
+    T GetList<T>(string key);
+    string[] GetListAll(string key);
+    T[] GetListAll<T>(string key);
+
+    long SetAdd(string key, object value);
+    string[] GetSet(string key);
+    T[] GetSet<T>(string key);
 }
--- Ext/Collapsenav.Net.Tool.Redis/RedisCache.cs
+++ Ext/Collapsenav.Net.Tool.Redis/RedisCache.cs
@@ -13,7 +13,6 @@ public sealed class FreeRedisCache : IRedisCache
     {
         return $"key_{GetType().Name}_{key}";
     }
-
     public string BuildHashKey(string key)
     {
         return $"hash_key_{GetType().Name}_{key}";
@@ -55,5 +54,52 @@ public sealed class FreeRedisCache : IRedisCache
     {
         return _client.HGet(BuildHashKey(key), BuildCacheKey(field));
     }
-
+    public long SetList(string key, params object[] value)
+    {
+        return _client.LPush(BuildCacheKey(key), value);
+    }
+    public string GetList(string key)
+    {
+        return _client.LIndex(BuildCacheKey(key), 0);
+    }
+    public string[] GetListAll(string key)
+    {
+        var cacheKey = BuildCacheKey(key);
+        var listLen = _client.LLen(cacheKey);
+        return _client.LRange(cacheKey, 0, listLen);
+    }
+    public T GetHash<T>(string key, string field)
+    {
+        return _client.HGet<T>(BuildHashKey(key), BuildCacheKey(field));
+    }
+    public T GetList<T>(string key)
+    {
+        return _client.LIndex<T>(BuildCacheKey(key), 0);
+    }
+    public T[] GetListAll<T>(string key)
+    {
+        var cacheKey = BuildCacheKey(key);
+        var listLen = _client.LLen(cacheKey);
+        return _client.LRange<T>(cacheKey, 0, listLen);
+    }
+    public Dictionary<string, string> GetHashAll(string key)
+    {
+        return _client.HGetAll(BuildHashKey(key));
+    }
+    public Dictionary<string, T> GetHashAll<T>(string key)
+    {
+        return _client.HGetAll<T>(BuildHashKey(key));
+    }
+    public long SetAdd(string key, params object[] value)
+    {
+        return _client.SAdd(key, value);
+    }
+    public string[] GetSet(string key)
+    {
+        return _client.SMembers(key);
+    }
+    public T[] GetSet<T>(string key)
+    {
+        return _client.SMembers<T>(key);
+    }
 }
